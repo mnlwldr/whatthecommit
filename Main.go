8286a15ba44c2b8d843dc9d754cd8a9aa806875c
mnlwldr <m.wildauer@gmail.com>
2020-08-04 16:11:50 +0200

    Init

---
+++ Main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	url := "http://whatthecommit.com/index.txt"
+	whatThecommitClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, getErr := whatThecommitClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	fmt.Printf("%s", string(body))
+
+}
