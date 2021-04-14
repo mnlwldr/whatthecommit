728215acff08cba73e1427ef0a44736f96dbf3a2
mnlwldr <m.wildauer@gmail.com>
2021-04-14 15:00:23 +0200

    :wq!

--- whatthecommit.go
+++ whatthecommit.go
@@ -1,6 +1,8 @@
 package whatthecommit
 
 import (
+	"errors"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,28 +12,38 @@ import (
 
 const url string = "http://whatthecommit.com/index.txt"
 
-// Return a the string from http://whatthecommit.com/index.txt
+// Message Return a the string from http://whatthecommit.com/index.txt
 func Message() string {
 
 	whatTheCommitClient := http.Client{
 		Timeout: time.Second * 2,
 	}
+
 	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
+
 	res, getErr := whatTheCommitClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
+	checkError(getErr)
+
+	if res.StatusCode != 200 {
+		checkError(errors.New(res.Status))
 	}
 
 	if res.Body != nil {
-		defer res.Body.Close()
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			checkError(err)
+		}(res.Body)
 	}
 
 	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
+	checkError(readErr)
+
 	return strings.Trim(string(body), "\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
